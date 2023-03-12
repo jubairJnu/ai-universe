c04128e18a3827ccb10c82334acf83cfbf5ac9ed
jubairJnu <jubair2810@gmail.com>
2023-03-12 22:04:43 +0600

    show-all

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ const loadData = () =>{
 const displayData = tools =>{
   console.log(tools);
 
+  // show all button\
+  const showall = document.getElementById('show-all');
+if(tools.length > 6){
+  tools= tools.slice(0,6);
+  showall.classList.remove('d-none');
+}
+else{showall.classList.add('d-none')}
+
   const dataContainer = document.getElementById('data-container');
   tools.forEach(tool =>{
 
@@ -27,12 +35,14 @@ const displayData = tools =>{
       <li>${tool.features[2]}</li>
       <ol>
     </div>
-    <div class="card-footer">
+    <div class="card-footer ">
     <h5 class="card-title">${tool.name}</h5>
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar" viewBox="0 0 16 16">
+    
+   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar" viewBox="0 0 16 16">
   <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/>
-</svg></i><span> ${tool.published_in
-}</span>  <span class="text-right"> <i class="fa-solid fa-arrow-right"></i></span>
+</svg></i> <span> ${tool.published_in
+}</span>  
+<button class="ms-5"> <i class="fa-solid fa-arrow-right"></i></button>
 
     </div>
   </div>`
